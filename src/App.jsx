1d29bd0e6023f550f92de1822b2f872c4406dbe9
LeaCrazyBambi <lheynitz@gmail.com>
2021-07-20 15:53:06 +0200

    With Joels help

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,44 @@
+import { useState } from "react"
+
 export default function App() {
-    return "🦆";
+    const [addresses, setAddresses] = useState([])
+    // const name = "Lea H."setName(event.target.value)
+    const [name, setName] = useState("Hans") 
+
+    const [number, setNumber] = useState("176")
+    return (
+        <div>
+        <form onSubmit={
+            (event) => {
+                event.preventDefault()
+                setAddresses([...addresses, {name: name, number: number}])
+            }
+        }>
+            <input type="text" value={name} onChange={
+                (event) => {
+                    setName(event.target.value)
+                                
+            }}></input>
+            <br></br>
+            <input type="text" value={number} onChange={        
+                (event) => {
+                    setNumber(event.target.value)
+            }}></input>
+            <br></br>
+
+            <button>Save!</button>
+        </form> 
+        <ul>
+            <li>Laura 05765</li>
+            <li>Lou 987654</li>
+            {addresses.map((address) => {
+                return <li>
+                    {address.name} {address.number}
+                </li>
+            })}
+        </ul>
+        </div>
+    )
 }
+
+
